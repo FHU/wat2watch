6a540fd27f055fdbd8babe7852ac79d3b3613a4c
Spencer Christian <spencer.christian@students.fhu.edu>
2015-10-20 16:03:21 -0500

    testing first commit with comment

---
+++ wat2watch/wat2watch/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  wat2watch
+//
+//  Created by CIS Student on 10/20/15.
+//  Copyright (c) 2015 sChristian. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //test the commit
+
+}
+
---
+++ wat2watch/wat2watch/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  wat2watch
+//
+//  Created by CIS Student on 10/20/15.
+//  Copyright (c) 2015 sChristian. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
